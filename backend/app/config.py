5e18ac116e7082e651b6629672e3f39be415b58d
Jhaymag <josephkkmmj@gmail.com>
2025-11-30 09:31:58 +0000

    Update origin cors

--- backend/app/config.py
+++ backend/app/config.py
@@ -45,8 +45,8 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://localhost:3000",
         "https://corepro.netlify.app",
-        "*"
     ]
+
     CORS_ALLOW_CREDENTIALS: bool = True
     CORS_ALLOW_METHODS: List[str] = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
     CORS_ALLOW_HEADERS: List[str] = ["*"]
